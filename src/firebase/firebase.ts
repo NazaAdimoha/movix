7f2a345cb9099381c756ea7567ddffc92cc5bb11
nazaadimoha <nazaadimoha98@gmail.com>
2023-01-15 14:08:33 +0100

    Update: added firebase config file with it's respective type

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,17 @@
+type FirebaseConfig = {
+    apiKey: string;
+    authDomain: string;
+    projectId: string;
+    storageBucket: string;
+    messagingSenderId: string;
+    appId: string;
+}
+
+export const firebaseConfig: FirebaseConfig = {
+    apiKey: "AIzaSyBDPaIZD_-RE0UgeVFjYcwSx47rQxU0ge0",
+    authDomain: "movix-aa85a.firebaseapp.com",
+    projectId: "movix-aa85a",
+    storageBucket: "movix-aa85a.appspot.com",
+    messagingSenderId: "136694868474",
+    appId: "1:136694868474:web:41b3588566659344e3dc43"
+  };
